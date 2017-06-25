d7aff6cd27e9e951b7383919d16c86b00b222f5c
tony_kero <antoine.karcher.id@gmail.com>
2017-06-25 02:38:57 +0200

    From interpreter to JIT Compiler

--- include/Metasm.hpp
+++ include/Metasm.hpp
@@ -1,14 +1,36 @@
 #pragma once
 
-#include <unordered_map>
+#include <libgccjit++.h>
+
 #include <vector>
 #include <stack>
 
 #include <functional>
 #include <utility>
 #include <sstream>
-#include <cstdlib>
+#include <unordered_map>
 #include <cmath>
+
+#define INST_PUSH    0x01
+#define INST_POP     0x02
+#define INST_SWAP    0x03
+#define INST_DUP     0x04
+#define INST_ADD     0x05
+#define INST_SUB     0x06
+#define INST_MUL     0x07
+#define INST_DIV     0x08
+#define INST_XOR     0x09
+#define INST_OR      0x0A
+#define INST_AND     0x0B
+#define INST_NOT     0x1C
+#define INST_SHR     0x1D
+#define INST_SHL     0x1E
+#define INST_LBL     0x1F
+#define INST_JMP     0x10
+#define INST_JE      0x11
+#define INST_JNE     0x12
+#define INST_JGE     0x13
+#define INST_JLE     0x14
 
 namespace meta
 {
@@ -16,48 +38,53 @@ namespace meta
 class Engine
 {
     public:
-        Engine  ();
+        Engine  ( unsigned int _maxStackDepth );
         ~Engine ();
 
-        bool                        isValid     ( const std::string& _script );
-        void                        loadScript  ( const std::string& _script );
-        void                        run         ();
+        void                        init_blocks();
 
-        const std::stack<int>&      getStack    () const;
-
-        void                        push        ( int _arg );
-        void                        pop         ();
-        void                        swap        ();
-        void                        dup         ();
-        void                        add         ();
-        void                        mul         ();
-        void                        div         ();
-        void                        nand        ();
-        void                        shf         ();
-        // void                        lbl         ( const std::string& _label );
-        void                        cmp         ();
-        void                        jmp         ( const std::string& _label );
-        void                        je          ( const std::string& _label );
-        void                        jne         ( const std::string& _label );
-        void                        jge         ( const std::string& _label );
-        void                        jle         ( const std::string& _label );
+        bool                        is_valid     ( const std::string& _script );
+        void                        load_script  ( const std::string& _script );
+        void                        compile      ();
+        int                         execute      ();
 
     private:
-        std::pair<int, int>         poplast2    ();
+
+        void                        push        ( gccjit::rvalue _arg );
+        void                        pop         ( gccjit::lvalue _arg );
+
         std::vector<std::string>    split       ( const std::string& _script, const char& _delim );
-        void                        parseLabels ();
+        unsigned int                scan        ( const std::string& );
+        void                        parse_labels();
 
+        std::string                 m_script;
 
-        std::unordered_map<std::string, std::function<void(const std::string&)>> m_instructions;
+        gccjit::context             m_context;
+        gccjit::type                m_int_type,
+                                    m_stack_type,
+                                    m_void_type,
+                                    m_void_ptr_type;
+        std::vector<gccjit::param>  m_params;
+        gccjit::function            m_function;
+
+        gcc_jit_result*             m_result;
         
-        std::unordered_map<std::string, unsigned int>   m_labels;
-        std::stack<int>                                 m_stack;
-        std::string                                     m_script;
-        unsigned int                                    m_ip = 0;
+        gccjit::lvalue              m_stack,
+                                    m_stack_depth,
+                                    m_x,
+                                    m_y,
+                                    m_ret;
+        gccjit::rvalue              m_const_one;
 
-        bool    isEqual     = false,
-                isLower     = false,
-                isGreater   = false;
+        gccjit::block               m_initial_block;
+        gccjit::block               m_actual_block, m_next_block;
+        bool                        m_valid_next = true;
+        
+        std::unordered_map< std::string, unsigned int >
+                                    m_labels;
+        std::vector<gccjit::block>  m_instruction_blocks;
+        
+        std::vector<std::string>    m_ops;
 };
 
 }
--- src/Metasm.cpp
+++ src/Metasm.cpp
@@ -2,194 +2,230 @@
 
 using namespace meta;
 
-Engine::Engine()
+Engine::Engine( unsigned int _maxStackDepth )
+:m_context      ( gccjit::context::acquire() ),
+m_int_type      ( m_context.get_type( GCC_JIT_TYPE_INT )),
+m_stack_type    ( m_context.new_array_type( m_int_type, _maxStackDepth )),
+m_void_type     ( m_context.get_type( GCC_JIT_TYPE_VOID ) ),
+m_void_ptr_type ( m_context.get_type( GCC_JIT_TYPE_VOID_PTR ) ),
+m_params        (1, m_context.new_param(m_int_type, "arg")),
+m_function      ( m_context.new_function( GCC_JIT_FUNCTION_EXPORTED, m_int_type, "fun", m_params, 0)),
+m_stack         ( m_function.new_local( m_stack_type, "stack" )),
+m_stack_depth   ( m_function.new_local( m_int_type, "stack_depth" )),
+m_x             ( m_function.new_local( m_int_type, "x" )),
+m_y             ( m_function.new_local( m_int_type, "y" )),
+m_ret           ( m_function.new_local( m_int_type, "ret" )),
+m_const_one     ( m_context.one(m_int_type)),
+m_initial_block ( m_function.new_block("initial"))
 {
-    m_instructions["PUSH"]  = [this]( const std::string& _str ){ push( atoi(_str.c_str()) );};
-    m_instructions["POP"]   = [this]( const std::string& _str ){ pop();             };
-    m_instructions["SWAP"]  = [this]( const std::string& _str ){ swap();            };
-    m_instructions["DUP"]   = [this]( const std::string& _str ){ dup();             };
-    m_instructions["ADD"]   = [this]( const std::string& _str ){ add();             };
-    m_instructions["MUL"]   = [this]( const std::string& _str ){ mul();             };
-    m_instructions["DIV"]   = [this]( const std::string& _str ){ div();             };
-    m_instructions["NAND"]  = [this]( const std::string& _str ){ nand();            };
-    m_instructions["SHF"]   = [this]( const std::string& _str ){ shf();             };
-    // m_instructions["LBL"]   = [this]( const std::string& _str ){ lbl( _str );       };
-    m_instructions["CMP"]   = [this]( const std::string& _str ){ cmp();             };
-    m_instructions["JMP"]   = [this]( const std::string& _str ){ jmp( _str );       };
-    m_instructions["JE"]    = [this]( const std::string& _str ){ je( _str );        };
-    m_instructions["JNE"]   = [this]( const std::string& _str ){ jne( _str );       };
-    m_instructions["JGE"]   = [this]( const std::string& _str ){ jge( _str );       };
-    m_instructions["JLE"]   = [this]( const std::string& _str ){ jle( _str );       };
+    m_context.set_bool_option   (GCC_JIT_BOOL_OPTION_DUMP_GENERATED_CODE, 0);
+    m_context.set_int_option    (GCC_JIT_INT_OPTION_OPTIMIZATION_LEVEL, 3);
+
 }
 
 Engine::~Engine()
 {
-
+    m_context.release();
+    gcc_jit_result_release( m_result );
 }
 
-bool Engine::isValid( const std::string& _script )
+void Engine::init_blocks()
+{
+    for(unsigned int i = 0; i < m_ops.size(); i++)
+    {
+        m_instruction_blocks.push_back( m_function.new_block( std::string( "insn" ) +
+                                        std::to_string(i) ));
+    }
+
+    m_initial_block.add_assignment( m_stack_depth, m_context.zero( m_int_type ) );
+    m_initial_block.end_with_jump( m_instruction_blocks[0] );
+}
+
+bool Engine::is_valid( const std::string& _script )
 {
     //TODO: isValid()
     return true;
 }
 
-void Engine::loadScript( const std::string& _script )
+void Engine::load_script( const std::string& _script )
 {
-    if(isValid(_script))
+    if(is_valid(_script))
     {
         m_script = _script;
-        parseLabels();
+    
+        std::vector<std::string> lines = split(m_script, '\n');
+        m_ops = lines;
+
+        init_blocks();
+        parse_labels();
     }
 }
 
-void Engine::run( )
+void Engine::compile( )
 {
-    std::vector<std::string> lines = split(m_script, '\n');
-
-    for(m_ip = 0; m_ip < lines.size(); m_ip++)
+    for(unsigned int i = 0; i < m_ops.size(); i++)
     {
-        std::vector<std::string> args = split(lines[m_ip], ' ');
-        std::string arg = "\0";
-
+        std::vector<std::string> args = split(m_ops[i], ' ');
+        std::string arg;
+    
+        // assign argument
         if(args.size() > 1)
             arg = args[1];
-        if(args[0] != "LBL")
-            m_instructions[ args[0] ](arg);
+        
+        m_actual_block  = m_instruction_blocks[i];
+        if(i < m_ops.size()-1)
+            m_next_block = m_instruction_blocks[i + 1];
+        else
+            m_valid_next = false;
+
+        unsigned int instruction_id = scan( args[0] );
+        switch( instruction_id )
+        {
+            case INST_PUSH:
+                push( m_context.new_rvalue( m_int_type, std::stoi( args[1] ) ));
+                break;
+            case INST_POP:
+                m_actual_block.add_assignment_op( m_stack_depth, GCC_JIT_BINARY_OP_MINUS, m_const_one );
+                break;
+            case INST_SWAP:
+                pop(m_x);
+                pop(m_y);
+                push(m_x);
+                push(m_y);
+                break;
+            case INST_DUP:
+                pop(m_x);
+                push(m_x);
+                push(m_x);
+                push(m_x);
+                break;
+            case INST_ADD:
+                pop(m_x);
+                pop(m_y);
+                push(m_x + m_y);
+                break;
+            case INST_SUB:
+                pop(m_x);
+                pop(m_y);
+                push(m_x - m_y);
+                break;
+            case INST_MUL:
+                pop(m_x);
+                pop(m_y);
+                push(m_x * m_y);
+                break;
+            case INST_DIV:
+                pop(m_x);
+                pop(m_y);
+                push(m_x / m_y);
+                break;
+            case INST_XOR:
+                pop(m_x);
+                pop(m_y);
+                push(m_x ^ m_y);
+                break;
+            case INST_OR:
+                pop(m_x);
+                pop(m_y);
+                push(m_x | m_y);
+                break;
+            case INST_AND:
+                pop(m_x);
+                pop(m_y);
+                push(m_x & m_y);
+                break;
+            case INST_NOT:
+                pop(m_x);
+                push(~m_x);
+                break;
+            case INST_SHR:
+                pop(m_y);
+                pop(m_x);
+                push( m_context.new_binary_op(GCC_JIT_BINARY_OP_RSHIFT, m_int_type, m_x, m_y));
+                break;
+            case INST_SHL:
+                pop(m_y);
+                pop(m_x);
+                push( m_context.new_binary_op(GCC_JIT_BINARY_OP_LSHIFT, m_int_type, m_x, m_y));
+                break;
+            case INST_LBL:
+                // do nothing
+                break;
+            case INST_JMP:
+                m_actual_block.end_with_jump( m_instruction_blocks[ m_labels[arg] ] );
+                break;
+            case INST_JE:
+                pop(m_x);
+                pop(m_y);
+                push(m_y);
+                push(m_x);
+                m_actual_block.end_with_conditional( m_x == m_y, m_instruction_blocks[ m_labels[arg] ], m_next_block );
+                break;
+            case INST_JNE:
+                pop(m_x);
+                pop(m_y);
+                push(m_y);
+                push(m_x);
+                m_actual_block.end_with_conditional( m_x != m_y, m_instruction_blocks[ m_labels[arg] ], m_next_block );
+                break;
+            case INST_JGE:
+                pop(m_x);
+                pop(m_y);
+                push(m_y);
+                push(m_x);
+                m_actual_block.end_with_conditional( m_x >= m_y, m_instruction_blocks[ m_labels[arg] ], m_next_block );
+                break;
+            case INST_JLE:
+                pop(m_x);
+                pop(m_y);
+                push(m_y);
+                push(m_x);
+                m_actual_block.end_with_conditional( m_x <= m_y, m_instruction_blocks[ m_labels[arg] ], m_next_block );
+                break;
+            default:
+                // instruction not found, can't be reached
+                break;
+        }
+    
+        if( instruction_id != INST_JMP &&
+            instruction_id != INST_JE &&
+            instruction_id != INST_JNE &&
+            instruction_id != INST_JGE &&
+            instruction_id != INST_JLE )
+        {
+            if(m_valid_next)
+                m_actual_block.end_with_jump( m_next_block );
+            else
+            {
+                m_actual_block.add_assignment_op( m_stack_depth, GCC_JIT_BINARY_OP_MINUS, m_const_one);
+                m_actual_block.add_assignment( m_ret, m_context.new_array_access( m_stack, m_stack_depth ) );
+                m_actual_block.end_with_return( m_ret );
+            }
+        }
+
     }
+    m_result = m_context.compile();
+    m_context.compile_to_file(GCC_JIT_OUTPUT_KIND_ASSEMBLER ,"SCRIPT");
 }
 
-const std::stack<int>& Engine::getStack() const
+int Engine::execute()
 {
-    return m_stack;
+    typedef int (*function)(int);
+    function fun = (function)gcc_jit_result_get_code(m_result, "fun");
+    return fun(1);
 }
 
 // Instructions
-void Engine::push( int _arg )
+void Engine::push( gccjit::rvalue _arg )
 {
-    m_stack.push( _arg );
+    m_actual_block.add_assignment( m_context.new_array_access( m_stack, m_stack_depth ), _arg );
+    m_actual_block.add_assignment_op( m_stack_depth, GCC_JIT_BINARY_OP_PLUS, m_const_one);
 }
 
-void Engine::pop()
+void Engine::pop( gccjit::lvalue _arg )
 {
-    m_stack.pop();
-}
-
-void Engine::swap()
-{
-    std::pair<int, int> ab = poplast2();
-            
-    m_stack.push(ab.first);
-    m_stack.push(ab.second);
-}
-
-void Engine::dup()
-{
-    m_stack.push( m_stack.top() );
-}
-
-void Engine::add()
-{
-    std::pair<int, int> ab = poplast2();
-
-    m_stack.push( ab.first + ab.second );
-}
-
-void Engine::mul()
-{
-    std::pair<int, int> ab = poplast2();
-
-    m_stack.push( ab.first * ab.second );
-}
-
-void Engine::div()
-{
-    std::pair<int, int> ab = poplast2();
-
-    m_stack.push( ab.first / ab.second );
-}
-
-void Engine::nand()
-{
-    std::pair<int, int> ab = poplast2();
-
-    int r = ab.first & ab.second;
-    m_stack.push( ~r );
-}
-
-void Engine::shf()
-{
-    std::pair<int, int> ab = poplast2();
-
-    if(ab.first >= 0)
-        m_stack.push(ab.second >> ab.first);
-    else if(ab.first < 0)
-        m_stack.push(ab.second << std::abs(ab.first));
-}
-
-void Engine::cmp()
-{
-    std::pair<int, int> ab = poplast2();
-    m_stack.push(ab.second);
-    m_stack.push(ab.first);
-
-    if(ab.first > ab.second)
-    {
-        isGreater   = true;
-        isLower     = false;
-        isEqual      = false;
-    }
-    else if(ab.first == ab.second)
-    {
-        isEqual     = true;
-        isLower     = false;
-        isGreater   = false;
-    }
-    else
-    {
-        isLower     = true;
-        isEqual     = false;
-        isGreater   = false;
-    }
-}
-
-void Engine::jmp( const std::string& _label )
-{
-   m_ip= m_labels[ _label ];
-}
-
-void Engine::je( const std::string& _label )
-{
-    if(isEqual)
-       m_ip= m_labels[ _label ];
-}
-
-void Engine::jne( const std::string& _label )
-{
-    if(!isEqual)
-       m_ip= m_labels[ _label ];
-}
-
-void Engine::jge( const std::string& _label )
-{
-    if(isGreater || isEqual)
-       m_ip= m_labels[ _label ];
-}
-
-void Engine::jle( const std::string& _label )
-{
-    if(isLower ||isEqual)
-       m_ip= m_labels[ _label ];
-}
-
-std::pair<int, int> Engine::poplast2()
-{
-    std::pair<int, int> ab;
-    ab.first    =   m_stack.top();
-                    m_stack.pop();
-    ab.second   =   m_stack.top();
-                    m_stack.pop();
-
-    return ab;
+    m_actual_block.add_assignment_op( m_stack_depth, GCC_JIT_BINARY_OP_MINUS, m_const_one );
+    m_actual_block.add_assignment( _arg, m_context.new_array_access( m_stack, m_stack_depth ) );
 }
 
 std::vector<std::string> Engine::split( const std::string& _script, const char& _delim )
@@ -204,14 +240,42 @@ std::vector<std::string> Engine::split( const std::string& _script, const char& 
     return ret;
 }
 
-void Engine::parseLabels()
+unsigned int Engine::scan( const std::string& _instruction )
 {
-    std::vector<std::string> lines = split(m_script, '\n');
-    
-    for(unsigned int i = 0; i < lines.size(); i++)
+    unsigned int inst_id = 0x00;
+    (_instruction == "PUSH")    && ( inst_id= INST_PUSH );
+    (_instruction == "POP")     && ( inst_id= INST_POP );
+    (_instruction == "SWAP")    && ( inst_id= INST_SWAP );
+    (_instruction == "DUP")     && ( inst_id= INST_DUP );
+    (_instruction == "ADD")     && ( inst_id= INST_ADD );
+    (_instruction == "SUB")     && ( inst_id= INST_SUB );
+    (_instruction == "MUL")     && ( inst_id= INST_MUL );
+    (_instruction == "DIV")     && ( inst_id= INST_DIV );
+    (_instruction == "XOR")     && ( inst_id= INST_XOR );
+    (_instruction == "OR")      && ( inst_id= INST_OR );
+    (_instruction == "AND")     && ( inst_id= INST_AND );
+    (_instruction == "NOT")     && ( inst_id= INST_NOT );
+    (_instruction == "SHR")     && ( inst_id= INST_SHR );
+    (_instruction == "SHL")     && ( inst_id= INST_SHL );
+    (_instruction == "LBL")     && ( inst_id= INST_LBL );
+    (_instruction == "JMP")     && ( inst_id= INST_JMP );
+    (_instruction == "JE")      && ( inst_id= INST_JE );
+    (_instruction == "JNE")     && ( inst_id= INST_JNE );
+    (_instruction == "JGE")     && ( inst_id= INST_JGE );
+    (_instruction == "JLE")     && ( inst_id= INST_JLE );
+
+    return inst_id;
+
+}
+
+void Engine::parse_labels()
+{
+    for(unsigned int i = 0; i < m_ops.size(); i++)
     {
-        std::vector<std::string> words = split(lines[i], ' ');
-        if(words[0] == "LBL")
-            m_labels[ words[1] ] = i;
+        std::vector<std::string> args = split(m_ops[i], ' ');
+        std::string arg;
+
+        if( args[0] == "LBL")
+            m_labels[args[1]] = i;
     }
 }
